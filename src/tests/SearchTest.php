38cb52f54e0b8441d612d8d75efc493adbd924a4
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-02 16:05:43 -0300

    Criaçao de testes unitario basicos com o phpunit nos metodos de busca por cep versao2.0

--- src/tests/SearchTest.php
+++ src/tests/SearchTest.php
@@ -8,23 +8,12 @@ class SearchTest extends TestCase{
   /**
    * @dataProvider dadosTeste
    */
+
+   //ra requisição vão vim dois paramentros, o primeiro é uma string(cep) e  segundo o valor da consulta que é um array($esperado)
   public function testGetAdressFromZipCodeDeafaultUsage(string $input, array $esperado){
     //recomendasse usar o test antes do nome da função que se vai testar e o fim usar o Deafultusage
    $resultado = new Search();
    $resultado = $resultado->getAdressFromZipCode($input);//cep usado como parametro da busca
-
-   $esperado = [
-       "cep" => "01001-000",
-       "logradouro" => "Praça da Sé",
-       "complemento" => "lado ímpar",
-       "bairro" => "Sé",
-       "localidade" => "São Paulo",
-       "uf" => "SP",
-       "ibge" => "3550308",
-       "gia" => "1004",
-       "ddd" => "11",
-       "siafi" => "7107",
-   ];
 
    $this->assertEquals($esperado,$resultado);
   } 
